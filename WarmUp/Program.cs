380e42bf5491b08b04bfef59bbf54b2c61275db9
anshul.singhal <singhal.s01@gmail.com>
2024-06-13 04:40:13 +0530

    Reverse Vowels in a given string

--- WarmUp/Program.cs
+++ WarmUp/Program.cs
@@ -1,3 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using WarmUp;
+
+Console.WriteLine("Warm Up Section");
+
+Console.WriteLine("Reverse Vowels");
+ReverseVowels reverseVowels = new ReverseVowels();
+reverseVowels.solution("hello");
+reverseVowels.solution("AEIOU");
+reverseVowels.solution("DesignGUrus");
--- WarmUp/ReverseVowels.cs
+++ WarmUp/ReverseVowels.cs
@@ -2,5 +2,70 @@ namespace WarmUp;
 
 public class ReverseVowels
 {
-    
+    // Use two pointer to swap the place for vowels
+    // t: O(n), s: O(1)
+
+    private string vowelsList = "aeiouAEIOU";
+    public void solution(string str)
+    {
+        if (string.IsNullOrEmpty(str) || str.Length <= 1)
+        {
+            return;
+        }
+
+        //string is immutable so can't do char replace operation like s[start] = c
+        //converted to char array
+        char[] chars = str.ToCharArray(); 
+        Console.WriteLine("Original string: " + string.Join("",chars));
+        //HashSet<char> vowelsList = getVowels();// this can be avoided if use a simple string variable
+        int start = 0;
+        int end = str.Length - 1;
+        while(start<end) //two pointer algo generally works best with while loop
+        {
+            while (start<end && !vowelsList.Contains(chars[start]))
+            {
+                start++;
+            }
+            while (end>start && !vowelsList.Contains(chars[end]))
+            {
+                end--;
+            }
+
+            if (end > start)
+            {
+                resverseChar(chars, start, end);
+            }
+
+            start++;
+            end--;
+        }
+        
+        Console.WriteLine("modified string: " + string.Join("",chars));
+        
+    }
+
+    private void resverseChar(char[] chars, int start, int end)
+    {
+        (chars[end], chars[start]) = (chars[start], chars[end]);
+        // char temp = chars[end];
+        // chars[end] = chars[start];
+        // chars[start] = temp;
+    }
+
+    //because vowels can be lower and upper case better to have complete list to match  
+    private HashSet<char> getVowels()
+    {
+        HashSet<char> vowelsList = new HashSet<char>();
+        vowelsList.Add('a');
+        vowelsList.Add('A');
+        vowelsList.Add('e');
+        vowelsList.Add('E');
+        vowelsList.Add('i');
+        vowelsList.Add('I');
+        vowelsList.Add('o');
+        vowelsList.Add('O');
+        vowelsList.Add('u');
+        vowelsList.Add('U');
+        return vowelsList;
+    }
 }
