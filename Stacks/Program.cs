c982ee9129753806521c1e2d16c37096dc5d3a2b
anshul.singhal <singhal.s01@gmail.com>
2024-06-28 22:31:19 +0530

    Added stack project

---
+++ Stacks/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Stack;
+
+Console.WriteLine("Stack");
+StockSpan stockSpan = new StockSpan();
+Console.WriteLine(string.Join(",", stockSpan.Solution(new int[]{100,80,60,70,60,75,85})??new int[]{}));
---
+++ Stacks/StockSpan.cs
@@ -0,0 +1,61 @@
+namespace Stack;
+
+public class StockSpan
+{
+    public int[]? Solution(int[]? nums)
+    {
+        if (nums == null || nums.Length == 0)
+        {
+            return nums;
+        }
+
+        // the bewlo lopp should not be needed
+        if (nums.Length == 1)
+        {
+            return new int[] { 1 };
+        }
+
+        int[] result = new int[nums.Length];
+        int minValue = 1;
+        Stack<map> stack = new Stack<map>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (stack.Count == 0)
+            {
+                result[i] = minValue;
+            }
+
+            while (stack.Count > 0 && stack.Peek().value < nums[i])
+            {
+                stack.Pop();
+            }
+
+            if (stack.Count > 0)
+            {
+                result[i] = i-stack.Peek().index ;
+            }
+            else
+            {
+                result[i] = minValue;
+            }
+            stack.Push(new map(nums[i],i));
+
+        }
+
+
+        return result;
+    }
+
+    public class map
+    {
+        public int value;
+        public int index;
+
+        public map(int value, int index)
+        {
+            this.value = value;
+            this.index = index;
+        }
+    }
+
+}
