868fb30db6be423ffde699914e30f45447740a84
anshuls01 <singhal.s01@gmail.com>
2025-04-10 12:11:01 +0530

    added topological sort folder

---
+++ TopologicalSort/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TopologicalSort;
+
+Console.WriteLine("Topological Sort");
+TopologicalSortTemplate template = new TopologicalSortTemplate();
+template.Print(template.CreateAdjency(new int[][] { new int[] { 5, 2 }, new int[] { 5, 0 }, new int[] { 4, 0 }, new int[] { 4, 1 }, new int[] { 2, 3 }, new int[] { 3, 1 } }));
---
+++ TopologicalSort/TopologicalSortTemplate.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopologicalSort
+{
+    internal class TopologicalSortTemplate
+    {
+        public void Print(Dictionary<int, List<int>> adj)
+        { 
+            Stack<int> stack = new Stack<int>();
+            HashSet<int> visited = new HashSet<int>();
+
+            foreach (int key in adj.Keys)
+            {
+                TopologicalSortUtil(key, stack, visited, adj);
+            }
+            //print the topological sort values
+            while (stack.Count > 0)
+            { 
+                Console.WriteLine(stack.Pop());
+            }
+        }
+
+        private void TopologicalSortUtil(int key, Stack<int> stack, HashSet<int> visited, Dictionary<int, List<int>> adj)
+        {
+            if (visited.Contains(key))
+            {
+                return;
+            }
+            visited.Add(key);
+            foreach (int neighbor in adj[key])
+            {
+                TopologicalSortUtil(neighbor, stack, visited, adj);
+            }
+            stack.Push(key);
+        }
+
+        public Dictionary<int, List<int>> CreateAdjency(int[][] edges)
+        {
+            Dictionary<int, List<int>> adj = new Dictionary<int, List<int>>();
+            foreach (int[] edge in edges)
+            {
+                if (!adj.ContainsKey(edge[0]))
+                {
+                    adj[edge[0]] = new List<int>();
+                }
+                if (!adj.ContainsKey(edge[1]))
+                {
+                    adj[edge[1]] = new List<int>();
+                }
+                adj[edge[0]].Add(edge[1]);
+            }
+            return adj;
+        }
+    }
+}
